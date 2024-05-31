fcd4516e12506c84e8e9e5b876aa6394b5ca3bd6
zen-pb <111935236+zen-pb@users.noreply.github.com>
2024-05-31 21:56:16 +0800

    Add functionality to screen and buttons

--- assets/src/calculator.js
+++ assets/src/calculator.js
@@ -1,21 +1,65 @@
 document.getElementById("currentYear").textContent = new Date().getFullYear();
 
-let num1 = 0;
-let num2;
-let operator;
+const topScreen = document.querySelector("#topScreen");
+const mainScreen = document.querySelector("#mainScreen");
+const numberButtons = document.querySelectorAll("[data-number]");
+const operatorButtons = document.querySelectorAll("[data-operator]");
+const clearButton = document.querySelector("#clearBTN");
+const backspaceButton = document.querySelector("#backspaceBTN");
+const decimalPointButton = document.querySelector("#decimalPointBTN");
+const equalsButton = document.querySelector("#equalsBTN");
 
-function add(num1, num2) {
-  return num1 + num2;
-}
+document.addEventListener("DOMContentLoaded", () => {
+  mainScreen.textContent = "0";
 
-function subtract(num1, num2) {
-  return num1 - num2;
-}
+  numberButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      if (mainScreen.textContent === "0") {
+        mainScreen.textContent = "";
+      }
 
-function multiply(num1, num2) {
-  return num1 * num2;
-}
+      if (!(mainScreen.textContent.length > 13)) {
+        mainScreen.textContent += button.textContent;
+      } else {
+        alert("You can no longer add more numbers!");
+      }
+    });
+  });
 
-function divide(num1, num2) {
-  return num1 / num2;
-}
+  operatorButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      topScreen.textContent = button.textContent;
+    });
+  });
+
+  clearButton.addEventListener("click", () => {
+    mainScreen.textContent = "0";
+    topScreen.textContent = "";
+  });
+
+  backspaceButton.addEventListener("click", () => {
+    if (mainScreen.textContent !== "0") {
+      mainScreen.textContent = mainScreen.textContent.slice(
+        0,
+        mainScreen.textContent.length - 1
+      );
+      if (mainScreen.textContent.length === 0) {
+        mainScreen.textContent = "0";
+      }
+    }
+  });
+
+  decimalPointButton.addEventListener("click", () => {
+    if (!mainScreen.textContent.includes(".")) {
+      if (!(mainScreen.textContent.length > 13)) {
+        mainScreen.textContent += ".";
+      }
+    } else {
+      alert("You can no longer add more numbers!");
+    }
+  });
+
+  equalsButton.addEventListener("click", () => {
+    topScreen.textContent = "";
+  });
+});
