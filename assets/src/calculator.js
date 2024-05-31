bc82e0177c4eed91b00c039527d43b152ac421bc
zen-pb <111935236+zen-pb@users.noreply.github.com>
2024-05-31 20:42:43 +0800

    Create the four basic operations as functions

--- assets/src/calculator.js
+++ assets/src/calculator.js
@@ -1,1 +1,17 @@
 document.getElementById("currentYear").textContent = new Date().getFullYear();
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
