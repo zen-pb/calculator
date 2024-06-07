49c0d434fbb8bbd58a7cfb69fad809295b993f7b
zen-pb <111935236+zen-pb@users.noreply.github.com>
2024-06-07 20:09:21 +0800

    Add keyboard support

--- assets/src/calculator.js
+++ assets/src/calculator.js
@@ -9,105 +9,159 @@ const backspaceButton = document.querySelector("#backspaceBTN");
 const decimalPointButton = document.querySelector("#decimalPointBTN");
 const equalsButton = document.querySelector("#equalsBTN");
 
-document.addEventListener("DOMContentLoaded", () => {
-  let operator, num1, num2;
-  let operatorButtonClicked = false;
-  let firstOperation = false;
+let operator, num1, num2;
+let operatorButtonClicked = false;
+let firstOperation = false;
 
+document.addEventListener("DOMContentLoaded", () => {
   mainScreen.textContent = "0";
 
+  window.addEventListener("keydown", keyboardHandler);
+
   numberButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      if (mainScreen.textContent === "0") {
-        mainScreen.textContent = "";
-      }
-
-      if (firstOperation) {
-        mainScreen.textContent = "";
-        firstOperation = false;
-      }
-
-      if (
-        !(mainScreen.textContent.length > 13) &&
-        !(mainScreen.textContent === "ERR: OVERFLOW")
-      ) {
-        mainScreen.textContent += button.textContent;
-      }
-    });
+    button.addEventListener("click", () => numberHandler(button));
   });
 
   operatorButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      if (!(mainScreen.textContent === "ERR: OVERFLOW")) {
-        if (operatorButtonClicked) {
-          operator = button.textContent;
-          num2 = mainScreen.textContent;
-          mainScreen.textContent = operate(operator, num1, num2);
-          topScreen.textContent = num1 + " " + operator + " " + num2 + " =";
-
-          if (mainScreen.textContent === "ERR: OVERFLOW") {
-            topScreen.textContent = "";
-          }
-        } else {
-          operator = button.textContent;
-          num1 = mainScreen.textContent;
-          topScreen.textContent = mainScreen.textContent + button.textContent;
-          operatorButtonClicked = true;
-          firstOperation = true;
-        }
-      }
-    });
+    button.addEventListener("click", () => operationHandler(button));
   });
 
-  clearButton.addEventListener("click", () => {
-    operatorButtonClicked = false;
+  clearButton.addEventListener("click", clearScreen);
+
+  backspaceButton.addEventListener("click", backspace);
+
+  decimalPointButton.addEventListener("click", addDecimal);
+
+  equalsButton.addEventListener("click", calculate);
+});
+
+function keyboardHandler(event) {
+  const button = document.createElement("button");
+
+  if (event.key >= 0 && event.key <= 9) {
+    button.textContent = event.key;
+    numberHandler(button);
+  }
+
+  switch (event.key) {
+    case "+":
+      button.textContent = event.key;
+      operationHandler(button);
+      break;
+    case "-":
+      button.textContent = event.key;
+      operationHandler(button);
+      break;
+    case "*":
+      button.textContent = "×";
+      operationHandler(button);
+      break;
+    case "/":
+      button.textContent = "÷";
+      operationHandler(button);
+      break;
+    case ".":
+      addDecimal();
+      break;
+    case "Enter":
+      calculate();
+      break;
+    case "Backspace":
+      backspace();
+      break;
+    case "Escape":
+      clearScreen();
+      break;
+  }
+}
+
+function numberHandler(button) {
+  if (mainScreen.textContent === "0") {
+    mainScreen.textContent = "";
+  }
+
+  if (firstOperation) {
+    mainScreen.textContent = "";
     firstOperation = false;
-    mainScreen.textContent = "0";
-    topScreen.textContent = "";
-    operator = "";
-    num1 = "";
-    num2 = "";
-  });
+  }
 
-  backspaceButton.addEventListener("click", () => {
-    if (
-      mainScreen.textContent !== "0" &&
-      !(mainScreen.textContent === "ERR: OVERFLOW")
-    ) {
-      mainScreen.textContent = mainScreen.textContent.slice(
-        0,
-        mainScreen.textContent.length - 1
-      );
-      if (mainScreen.textContent.length === 0) {
-        mainScreen.textContent = "0";
-      }
-    }
-  });
+  if (
+    !(mainScreen.textContent.length > 13) &&
+    !(mainScreen.textContent === "ERR: OVERFLOW")
+  ) {
+    mainScreen.textContent += button.textContent;
+  }
+}
 
-  decimalPointButton.addEventListener("click", () => {
-    if (
-      !mainScreen.textContent.includes(".") &&
-      !(mainScreen.textContent === "ERR: OVERFLOW")
-    ) {
-      if (!(mainScreen.textContent.length > 13)) {
-        mainScreen.textContent += ".";
-      }
-    }
-  });
-
-  equalsButton.addEventListener("click", () => {
-    if (!(mainScreen.textContent === "ERR: OVERFLOW") && !firstOperation) {
+function operationHandler(button) {
+  if (!(mainScreen.textContent === "ERR: OVERFLOW")) {
+    if (operatorButtonClicked) {
+      operator = button.textContent;
       num2 = mainScreen.textContent;
       mainScreen.textContent = operate(operator, num1, num2);
       topScreen.textContent = num1 + " " + operator + " " + num2 + " =";
-      operatorButtonClicked = false;
-    }
 
-    if (mainScreen.textContent === "ERR: OVERFLOW") {
-      topScreen.textContent = "";
+      if (mainScreen.textContent === "ERR: OVERFLOW") {
+        topScreen.textContent = "";
+      }
+    } else {
+      operator = button.textContent;
+      num1 = mainScreen.textContent;
+      topScreen.textContent = mainScreen.textContent + button.textContent;
+      operatorButtonClicked = true;
+      firstOperation = true;
     }
-  });
-});
+  }
+}
+
+function clearScreen() {
+  operatorButtonClicked = false;
+  firstOperation = false;
+  mainScreen.textContent = "0";
+  topScreen.textContent = "";
+  operator = "";
+  num1 = "";
+  num2 = "";
+}
+
+function backspace() {
+  if (
+    mainScreen.textContent !== "0" &&
+    !(mainScreen.textContent === "ERR: OVERFLOW")
+  ) {
+    mainScreen.textContent = mainScreen.textContent.slice(
+      0,
+      mainScreen.textContent.length - 1
+    );
+    if (mainScreen.textContent.length === 0) {
+      mainScreen.textContent = "0";
+    }
+  }
+}
+
+function addDecimal() {
+  if (
+    !mainScreen.textContent.includes(".") &&
+    !(mainScreen.textContent === "ERR: OVERFLOW")
+  ) {
+    if (!(mainScreen.textContent.length > 13)) {
+      mainScreen.textContent += ".";
+    }
+  }
+}
+
+function calculate() {
+  if (!(mainScreen.textContent === "ERR: OVERFLOW") && !firstOperation) {
+    num2 = mainScreen.textContent;
+    mainScreen.textContent = operate(operator, num1, num2);
+    topScreen.textContent = num1 + " " + operator + " " + num2 + " =";
+    operatorButtonClicked = false;
+  }
+
+  if (mainScreen.textContent === "ERR: OVERFLOW") {
+    topScreen.textContent = "";
+  }
+}
 
 function operate(operator, num1, num2) {
   let answer;
@@ -150,7 +204,7 @@ function multiply(num1, num2) {
     return answer;
   }
 
-  return answer.toFixed(2);
+  return answer.toFixed(4);
 }
 
 function divide(num1, num2) {
